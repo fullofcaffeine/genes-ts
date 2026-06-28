650be6c39da1152b52954a51fa04c0d807dafe31
Marcelo Serpa <boss@fullofcaffeine.com>
2026-06-27 21:43:24 -0600

    test(ts): update todoapp Date metadata snapshots

--- examples/todoapp/web/dist-ts-lowlevel/src-gen/StdTypes.ts
+++ examples/todoapp/web/dist-ts-lowlevel/src-gen/StdTypes.ts
@@ -7,6 +7,8 @@ export interface ArrayAccess<T> {}
 declare global {
   interface StringConstructor { __name__?: string | boolean }
   interface ArrayConstructor { __name__?: string | boolean }
+  interface DateConstructor { __name__?: string | boolean }
+  interface Date { __class__?: Function }
   interface PositionError { readonly code: number; readonly message: string }
   const PositionError: { readonly PERMISSION_DENIED: 1; readonly POSITION_UNAVAILABLE: 2; readonly TIMEOUT: 3; readonly prototype: PositionError };
   interface FetchObserver { readonly state: "requesting" | "responding" | "aborted" | "errored" | "complete"; onstatechange: Function; onrequestprogress: Function; onresponseprogress: Function }
